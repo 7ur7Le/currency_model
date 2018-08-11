3a30c7d0005dbd1e5ec945e715bc5588156f0771
shubham <ss250892@gmail.com>
2018-08-12 01:39:03 +0530

    version compatibility fix

--- currency/urls.py
+++ currency/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-
+try:
+    from django.urls import include, path
+except ImportError:
+    from django.conf.urls import include
+    from django.conf.urls import url as path
 from . import views
+
 
 urlpatterns = [
     path('pair_detail/', views.pair_detail, name='pair_detail'),
